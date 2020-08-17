74abf9c6aee1f9201a4b330ab4edf32ec3d00ea9
Pedro Henrique <pedro.henrique59@outlook.com>
2020-08-17 17:17:30 -0300

    Implementando conexão h2

--- src/main/java/com/eumesmo/course/entities/User.java
+++ src/main/java/com/eumesmo/course/entities/User.java
@@ -2,6 +2,12 @@ package com.eumesmo.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -25,6 +31,8 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
